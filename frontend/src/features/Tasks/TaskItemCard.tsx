4ec632b95e41b1260584706a34ca1770e181197c
Guilherme <jg21504956@gmail.com>
2026-03-09 15:48:12 -0300

    fix(front): error when marking task as completed

--- frontend/src/features/Tasks/TaskItemCard.tsx
+++ frontend/src/features/Tasks/TaskItemCard.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react"
+
 import type { TaskItemProtocol } from "@/types/TaskTypes"
 import { Field , FieldDescription } from "@/components/ui/field"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Separator } from "@/components/ui/separator"
 
+
 export default function TaskItemCard({task} : {task : TaskItemProtocol}){
+    const [checked , setChecked] = useState<boolean>(task.status == "completed")
+
+    const MarkTask = () : void => setChecked(prevs => !prevs )
+
     return (
-        <Field key={task.id} className="w-full my-2 shadow shadow-neutral-200 hover:bg-neutral-200 group cursor-pointer rounded-[5px]">
+        <Field key={task.id} className="w-full my-2 shadow shadow-neutral-200 hover:bg-neutral-200 group cursor-pointer rounded-[5px]" onDoubleClick={()=>MarkTask()}>
             <h1 className="flex px-2 py-3 cursor-pointer text-2xl items-center">
-                <Checkbox className="mx-2 group-hover:border-black" checked={task.status == "completed"} />  {task.title}
+                <Checkbox className="mx-2 group-hover:border-black" checked={checked} onCheckedChange={()=>setChecked(prev=>!prev)} />  {task.title}
             </h1>
             <FieldDescription className="px-5 font-light">
                 {task.description}
